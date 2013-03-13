90f92848a357f90ec34d9eff34a4941fb6e96327
Robby Ronk <robby.ronk@gmail.org>
2013-03-13 15:54:40 -0400

    Tests wont run. argh.

--- src/test/java/org/cru/quickstart/HelloTest.java
+++ src/test/java/org/cru/quickstart/HelloTest.java
@@ -1,5 +1,7 @@
 package org.cru.quickstart;
 
+import org.cru.cdi.HttpObjectsProducer;
+import org.cru.cdi.RequestWebListener;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.testng.Arquillian;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
@@ -10,7 +12,11 @@ import org.jboss.shrinkwrap.resolver.api.maven.MavenDependencyResolver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import javax.inject.Inject;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class HelloTest extends Arquillian {
     @Deployment
@@ -20,11 +26,35 @@ public class HelloTest extends Arquillian {
                 .loadMetadataFromPom("pom.xml");
 
         return ShrinkWrap.create(WebArchive.class)
+                .addClasses(RequestWebListener.class, HttpObjectsProducer.class, InjectionTestServlet.class)
                 .addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml");
     }
 
     @Test
     public void simpleTest() {
+        try {
+            URL url = new URL("http://localhost:8080/arquillian-service/InjectionTest");
+            HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+            httpURLConnection.setRequestMethod("GET");
+            httpURLConnection.setDoOutput(true);
+            httpURLConnection.setReadTimeout(1000);
 
+            httpURLConnection.connect();
+
+            BufferedReader rd = new BufferedReader(new InputStreamReader(httpURLConnection.getInputStream()));
+            StringBuilder sb = new StringBuilder();
+
+            String line;
+            while ((line = rd.readLine()) != null) {
+                sb.append(line + '\n');
+            }
+
+            final String string = sb.toString();
+            Assert.assertTrue(string.contains("request"));
+            Assert.assertTrue(string.contains("response"));
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
---
+++ src/test/java/org/cru/quickstart/InjectionTestServlet.java
@@ -0,0 +1,38 @@
+package org.cru.quickstart;
+
+import javax.inject.Inject;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/InjectionTest")
+public class InjectionTestServlet extends HttpServlet {
+    @Inject
+    HttpServletRequest request;
+    @Inject
+    HttpServletResponse response;
+
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+
+            throws ServletException, IOException {
+
+        PrintWriter writer = resp.getWriter();
+
+        writer.println("<html><head /><body>");
+
+        if (request != null)
+            writer.println("<h1>request was injected</h1>");
+
+        if (response != null)
+            writer.println("<h1>response was injected</h1>");
+
+        writer.println("</body></html>");
+
+        writer.close();
+
+    }
+}
